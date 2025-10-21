539fad630c50885baed7c3c02c561424728802bd
Lia <29ldivcohen@gannacademy.org>
2025-10-21 10:15:14 -0400

    Done

--- src/App.java
+++ src/App.java
@@ -3,9 +3,13 @@ import processing.core.*;
 public class App extends PApplet {
     int circleX = 0;
     int circleY = 0;
+    int cirleSpeed = 5;
     int rectX = 0;
     int rectY = 0;
     int herox = 50;
+
+    float velocity;
+    float accelration;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -13,6 +17,8 @@ public class App extends PApplet {
 
     public void setup() {
 
+        accelration = .1f;
+        velocity = 3;
     }
 
     public void settings() {
@@ -21,20 +27,30 @@ public class App extends PApplet {
 
     public void draw() {
         background(100);
+        velocity += accelration;
+        circleY += velocity;
+        if (circleY >= 415) {
+            circleY = 415;
+            velocity = 0;
+        }
+        if (circleY >= 415) {
+             velocity = -10;
+        }
+        if (circleY <= 0 ){
+            velocity = 10;
+        }
+
         circle(250, circleY, 30);
-        circleY += 2;
         rect(herox, 430, 200, 50);
     }
 
     public void keyPressed() {
-        rect(herox, 430, 200, 50);
-        if (keyCode == LEFT){
-             herox -= 5;
-        }else if (keyCode == RIGHT){
-        herox += 5;
-     }
-        
-        
+
+        if (keyCode == LEFT) {
+            herox -= 10;
+        } else if (keyCode == RIGHT) {
+            herox += 10;
+        }
 
     }
 }
