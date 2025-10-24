2cbb0aae7c10cfb898051747a4fe479a859e521a
Lia <29ldivcohen@gannacademy.org>
2025-10-24 13:00:49 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,23 +1,31 @@
 import processing.core.*;
 
 public class App extends PApplet {
-    int circleX = 250;
-    int circleY = 250;
-    int cirleSpeed = 5;
+    int circleX, circleY;
+    int circleSpeed = 5;
     int heroy = 415;
     int herox = 50;
     int herow = 200;
+    int heroh = 50;
+
+    int increaseSpeed;
+
+    int scene = 1;
 
     float velocity;
     float accelration;
+
+    int score = 0;
 
     public static void main(String[] args) {
         PApplet.main("App");
     }
 
     public void setup() {
+        textSize(25);
         accelration = .1f;
-        velocity = 3;
+        int circleSpeed = 5;
+        resetGame();
     }
 
     public void settings() {
@@ -25,22 +33,46 @@ public class App extends PApplet {
     }
 
     public void draw() {
+        if (scene == 1) {
+            gameplay();
+              text ("Bounce: " + score, 0, 50);
+
+              text ("Circle Speed = " +   circleSpeed, 0, 75);
+              
+              
+
+        } else if (scene == 2) {
+            background(0);
+            text("Game over", 50, 250);
+            text("Press space bar to play again", 50, 285);
+        }
+    }
+
+    public void gameplay() {
         background(100);
         velocity += accelration;
         circleY += velocity;
-      
-        if (circleY >= heroy && circleX >= herox && circleX <= herox + herow) { // code: if it doesn't touch padel then just go off the screen 
-            print("bounce");
-             velocity = -10;
-        }
-       // if (circleY == 0) { 
-           // background (255);
-      //  }
-
-       
     
+        if (circleY >= heroy && circleX >= herox && circleX <= herox + herow) { // code: if it doesn't touch padel
+                                                                                // then just go off the screen
+            println("bounce");
+            velocity = -10;
+
+            println(score);
+            score ++;
+
+            println("increase speed");
+            circleSpeed = 5;
+            
+           
+        }
+
+        if (circleY >= 500) {
+            scene = 2;
+        }
+       
         circle(circleX, circleY, 30);
-        rect(herox, heroy, herow, 50);
+        rect(herox, heroy, herow, heroh);
     }
 
     public void keyPressed() {
@@ -50,6 +82,21 @@ public class App extends PApplet {
         } else if (keyCode == RIGHT) {
             herox += 10;
         }
+
+        if (key == ' ') {
+            scene = 1;
+            resetGame();
+
+        }
+    }
+
+    public void resetGame() {
+        circleX = 250;
+        circleY = 250;
+        velocity = 3;
+        herox = 50;
+        heroy = 415;
+        score = 0;
 
     }
 }
